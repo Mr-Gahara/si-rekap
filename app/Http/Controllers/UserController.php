0e6b852d9bc125471a1540cdda2c33b5aef71b33
= <=>
2024-07-12 15:10:22 +0700

    import/export

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use App\Imports\UsersImport;
+use App\Exports\UsersExport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class UserController extends Controller
 {
@@ -18,7 +21,12 @@ class UserController extends Controller
     */
     public function export() 
     {
-        // return Excel::download(new UsersExport, 'users.xlsx');
+        try {
+            return Excel::download(new UsersExport, 'users.xlsx');
+        } catch (\Exception $e) {
+            // Log the error or handle it as per your application's error handling strategy
+            return redirect()->back()->withErrors('Error exporting users.');
+        }
     }
          
     /**
@@ -26,6 +34,10 @@ class UserController extends Controller
     */
     public function import(Request $request) 
     {
-        
+        $this->validate($request,[
+            'file' => 'required'
+        ]);
+        Excel::import(new UsersImport, request()->file('file'));
+        return redirect()->back();
     }
 }
---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class UsersImport implements ToCollection
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) 
+        {
+            if($row['0'] != null && User::where('email',$row['1'])->doesntExist()) {
+                User::create([
+                    'name' => $row['0'],
+                    'email' => $row['1'],
+                    'password' => Hash::make($row['0']),
+                ]);
+            }
+        }
+    }
+}
